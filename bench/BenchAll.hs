9f81665df708b07daa887b89c55c1ffb9efd7590
Adam Wagner <awagner83@gmail.com>
2013-12-28 10:03:51 -0500

    more benchmarks.

--- bench/BenchAll.hs
+++ bench/BenchAll.hs
@@ -1,18 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.DeepSeq
 import Criterion.Main
-import Data.Text (pack)
+import Data.Binary
+import Data.ByteString.Lazy (ByteString)
+import Data.Text (Text, pack)
 import Data.Text.Identifiers
 
-benchRoundtrip :: Int -> Identifiers
-benchRoundtrip = fromList . genNames
-    where genNames n = map (pack . show) . take n $ ([100000000..] :: [Int])
+genNames :: Int -> [Text]
+genNames n = map (pack . show) . take n $ ([100000000..] :: [Int])
 
 main :: IO ()
-main = defaultMain [
-        bgroup "insert" [ bench "    1,000" $ nf benchRoundtrip 1000
-                        , bench "   10,000" $ nf benchRoundtrip 10000
-                        , bench "  100,000" $ nf benchRoundtrip 100000
-                        ]
+main = do
+    let setA = genNames 1000
+        setB = genNames 10000
+        setC = genNames 100000
+        idA = fromList setA
+        idB = fromList setB
+        idC = fromList setC
+        encA = encode idA
+        encB = encode idB
+        encC = encode idC
+    defaultMain
+        [ bgroup "fromList"
+            [ setA `deepseq` bench "  1,000" $ nf fromList setA
+            , setB `deepseq` bench " 10,000" $ nf fromList setB
+            , setC `deepseq` bench "100,000" $ nf fromList setC
+            ]
+        , bgroup "lookupKey"
+            [ idA `deepseq` bench "  1,000" $ nf (flip lookupKey 500)   idA
+            , idB `deepseq` bench " 10,000" $ nf (flip lookupKey 5000)  idB
+            , idC `deepseq` bench "100,000" $ nf (flip lookupKey 50000) idC
+            ]
+        , bgroup "lookupId"
+            [ bench "  1,000" $ nf (flip lookupId "100000500") idA
+            , bench " 10,000" $ nf (flip lookupId "100005000") idB
+            , bench "100,000" $ nf (flip lookupId "100050000") idC
+            ]
+        , bgroup "encode"
+            [ bench "  1,000" $ nf encode idA
+            , bench " 10,000" $ nf encode idB
+            , bench "100,000" $ nf encode idC
+            ]
+        , bgroup "decode"
+            [ encA `deepseq` bench "  1,000" $ nf (decode :: ByteString -> Identifiers) encA
+            , encB `deepseq` bench " 10,000" $ nf (decode :: ByteString -> Identifiers) encB
+            , encC `deepseq` bench "100,000" $ nf (decode :: ByteString -> Identifiers) encC
+            ]
         ]
 
