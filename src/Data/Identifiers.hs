e8578075f7c73031787c976cf0cf5746846c9578
Adam Wagner <awagner83@gmail.com>
2014-01-04 17:33:55 -0500

    Identifiers 'id' type can now be any Integral type, instead of just Int.

--- src/Data/Identifiers.hs
+++ src/Data/Identifiers.hs
@@ -44,95 +44,96 @@ import qualified Data.HashMap.Lazy as H
 import qualified Data.Sequence as S
 
 
-data Identifiers a = Identifiers { ids   :: !(HashMap a Int)
-                                 , names :: !(Seq a)
-                                 } deriving Eq
+data Identifiers i a = Identifiers { ids   :: !(HashMap a i)
+                                   , names :: !(Seq a)
+                                   } deriving Eq
 
-instance Show a => Show (Identifiers a) where
+instance Show a => Show (Identifiers i a) where
     show s = "insertMany empty " ++ show (H.keys (ids s))
 
-instance (Eq a, Hashable a, Binary a) => Binary (Identifiers a) where
+instance (Binary i, Eq a, Hashable a, Binary a) => Binary (Identifiers i a) where
     put s = put (H.toList $ ids s) >> put (names s)
     get = Identifiers <$> (H.fromList <$> get) <*> get
 
-instance NFData a => NFData (Identifiers a) where
+instance (NFData i, NFData a) => NFData (Identifiers i a) where
     rnf (Identifiers i n) = rnf (i, n)
 
 -- | The empty Identifiers
-empty :: Identifiers a
+empty :: Identifiers i a
 empty = Identifiers H.empty S.empty
 
 -- | New Identifiers from list
-fromList :: (Hashable a, Eq a) => [a] -> Identifiers a
+fromList :: (Integral i, Hashable a, Eq a) => [a] -> Identifiers i a
 fromList = insertMany empty
 
 -- | Insert item into set (given it a new id)
-insert :: (Hashable a, Eq a) => Identifiers a -> a -> Identifiers a
+insert :: (Integral i, Hashable a, Eq a) => Identifiers i a -> a -> Identifiers i a
 insert xs v = case H.lookup v (ids xs) of
         Just _  -> xs
         Nothing -> Identifiers (H.insert v next $ ids xs) (names xs |> v)
-    where next = S.length (names xs)
+    where next = fromIntegral $ S.length $ names xs
 
 -- | Insert many items into set
-insertMany :: (Hashable a, Eq a) => Identifiers a -> [a] -> Identifiers a
+insertMany :: (Integral i, Hashable a, Eq a) => Identifiers i a -> [a] -> Identifiers i a
 insertMany = foldl' insert
 
 -- | New List from Identifiers
-toList :: Identifiers a -> [a]
+toList :: Identifiers i a -> [a]
 toList = H.keys . ids
 
 -- | Find id for given key
-lookupId :: (Hashable a, Eq a) => Identifiers a -> a -> Maybe Int
+lookupId :: (Hashable a, Eq a) => Identifiers i a -> a -> Maybe i
 lookupId = flip H.lookup . ids
 
-size :: Identifiers a -> Int
+size :: Identifiers i a -> Int
 size = S.length . names
 
-unsafeLookupId :: (Hashable a, Eq a) => Identifiers a -> a -> Int
+unsafeLookupId :: (Hashable a, Eq a) => Identifiers i a -> a -> i
 unsafeLookupId = (H.!) . ids
 
 -- | Find key for given id
-lookupKey :: Identifiers a -> Int -> Maybe a
+lookupKey :: (Integral i) => Identifiers i a -> i -> Maybe a
 lookupKey ident x = let xs = names ident
-                    in if S.length xs < x
+                    in if S.length xs < fromIntegral x
                        then Nothing
                        else Just $ unsafeLookupKey ident x
 
 -- | Given many ids, return many keys
-lookupKeys :: Identifiers a -> [Int] -> [a]
+lookupKeys :: (Integral i) => Identifiers i a -> [i] -> [a]
 lookupKeys s = mapMaybe (lookupKey s)
 
-unsafeLookupKey :: Identifiers a -> Int -> a
-unsafeLookupKey = S.index . names
+unsafeLookupKey :: Integral i => Identifiers i a -> i -> a
+unsafeLookupKey xs x = S.index (names xs) (fromIntegral x)
 
-(!) :: Identifiers a -> Int -> a
+(!) :: Integral i => Identifiers i a -> i -> a
 (!) = unsafeLookupKey
 
 -- | Items inserted are given ids
 prop_hasId :: String -> Bool
-prop_hasId x = isJust . lookupId (insert empty x) $ x
+prop_hasId x = isJust . lookupId (insert (empty :: Identifiers Int String) x) $ x
 
 -- | Inserted items have stable ids
 prop_stableId :: String -> Bool
 prop_stableId x = isJust a && a == b
     where a = lookupId firstSet x
           b = lookupId secondSet x
-          firstSet = insert empty x
+          firstSet = insert (empty :: Identifiers Int String) x
           secondSet = insert firstSet x
 
 -- | Given id can be used to fetch inserted item
 prop_keyRetrievalUnsafe :: [String] -> Bool
 prop_keyRetrievalUnsafe xs = all (\x -> ret x == x) xs
     where ret = unsafeLookupKey s . unsafeLookupId s
-          s = insertMany empty xs
+          s = insertMany (empty :: Identifiers Int String) xs
 
 -- | Given id can be used to fetch inserted item
 prop_keyRetrieval :: [String] -> Bool
 prop_keyRetrieval xs = all (\x -> ret x == Just (Just x)) xs
     where ret x = lookupKey s <$> lookupId s x
-          s = insertMany empty xs
+          s = insertMany (empty :: Identifiers Int String) xs
 
 -- | Inserting something more than once does not change the set
 prop_idempotent :: String -> Bool
-prop_idempotent x = insert empty x == insert (insert empty x) x
+prop_idempotent x = insert (empty :: Identifiers Int String) x
+                        == insert (insert empty x) x
 
